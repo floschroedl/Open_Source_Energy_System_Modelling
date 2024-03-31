93c1caa6c7f2e37a3b7398199f44ce4a4a8c477e
floschroedl <e1428401@student.tuwien.ac.at>
2024-03-31 15:33:22 +0200

    Edit Calculation File

---
+++ Calculation.py
@@ -0,0 +1,16 @@
+
+def add(a,b):
+    # add a and b and return result
+    return a + b
+
+def sub(a,b):
+    # subtract b form a and return result
+    return a - b
+
+def div (a,b):
+    # divide a by b and return result
+    return a / b
+
+def mul(a,b):
+    # multiplie a with b and return result
+    return a * b
